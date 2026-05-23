c1ebd4fd198cca2c61c499281c32f6713a741b6d
Osset1 <145881905+Osset1@users.noreply.github.com>
2026-05-23 19:45:48 -0400

    render caca

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'scene', loadComponent: () => import('./scene.component').then(m => m.SceneComponent) },
+  { path: 'stage', loadComponent: () => import('./components/stage/stage').then(m => m.Stage) }
+]
