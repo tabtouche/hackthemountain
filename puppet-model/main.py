8091e67b74bd3472443b08fdbb4445841e1a3acb
Thierry-git <thierry.poulin000@gmail.com>
2026-05-23 22:26:58 -0400

    feat(puppet model): demo training code

--- puppet-model/main.py
+++ puppet-model/main.py
@@ -6,6 +6,11 @@ from mediapipe_model_maker import gesture_recognizer
 
 import matplotlib.pyplot as plt
 
+
+#############################################################
+# Get the dataset
+#############################################################
+
 dataset_path = "dataset/rps_data_sample"
 
 print(dataset_path)
@@ -15,3 +20,59 @@ for i in os.listdir(dataset_path):
     labels.append(i)
 print(labels)
 
+if (not os.path.exists("preview")):
+  os.makedirs("preview")
+
+NUM_EXAMPLES = 5
+
+for label in labels:
+  label_dir = os.path.join(dataset_path, label)
+  example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
+  fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10,2))
+  for i in range(NUM_EXAMPLES):
+    axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
+    axs[i].get_xaxis().set_visible(False)
+    axs[i].get_yaxis().set_visible(False)
+  fig.suptitle(f'Showing {NUM_EXAMPLES} examples for {label}')
+  plt.savefig(f"preview/preview_{label}.png")
+
+
+#############################################################
+# Load the dataset
+#############################################################
+
+data = gesture_recognizer.Dataset.from_folder(
+    dirname=dataset_path,
+    hparams=gesture_recognizer.HandDataPreprocessingParams()
+)
+train_data, rest_data = data.split(0.8)
+validation_data, test_data = rest_data.split(0.5)
+
+
+#############################################################
+# Train the model
+#############################################################
+
+hparams = gesture_recognizer.HParams(export_dir="exported_model")
+hparams.epochs = 40
+options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
+model = gesture_recognizer.GestureRecognizer.create(
+    train_data=train_data,
+    validation_data=validation_data,
+    options=options
+)
+
+
+#############################################################
+# Evaluate the model performance
+#############################################################
+
+loss, acc = model.evaluate(test_data, batch_size=1)
+print(f"Test loss:{loss}, Test accuracy:{acc}")
+
+
+#############################################################
+# Evaluate the model performance
+#############################################################
+
+model.export_model()
